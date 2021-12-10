d3f650b4b2bb0b525cc9ef42c6557fe1f793159a
Abdo Yasser <61123689+thehohoman@users.noreply.github.com>
2021-12-10 22:48:18 +0200

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html5>
+<head>
+
+<title>Blood bank Management System</title>
+<link href="css/lightbox.css" rel="stylesheet" />
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+
+</head>
+
+<body>
+<?php include('admin/function.php'); ?>
+
+ <div class="h_bg">
+<div class="wrap">
+<div class="header">
+		<div class="logo">
+			<h1><a href="index.php"><img src="Images/logo.png" alt=""></a></h1>
+		</div>
+	</div>
+</div>
+</div>
+<div class="nav_bg">
+<div class="wrap">
+		<?php require('header.php');?>
+	</div>
+  
+   
+<div style="height:500px;">
+     <form method="post" enctype="multipart/form-data">
+<div class="s_bg">
+<div class="wrap">
+<div class="cont_main">
+	<div class="content">
+		<img src="Images/about.png" height="70px"  />
+			<h4><span class="bold">News Letter from the Founders</span></h4>
+			<p><img src="Images/123.jpg" height="200px" style="margin-bottom:40px" /></a>The 'Life Saver Blood Bank' is the first product to emerge from 'SV Technologies' community welfare effort, the 'Web Development Project.' 'Blood' is universally acknowledged as the most valuable ingredient that maintains life. It saves countless lives in a variety of situations all around the world. Someone, somewhere, is in severe need of blood once every two seconds. Every year, more than 29 million units of blood components are transfused. Blood is in high demand; roughly 39,000 units of Red Blood Cells are required on any given day. We could only meet about 1% (approximately) of our country's blood transfusion demand each year.</p>
+			<p class="top">Despite an increase in donor numbers, blood is still in short supply during emergencies, owing to a lack of awareness and accessibility. We are confident that by efficiently connecting blood donors with blood recipients, this technology will be able to address the majority of these issues. Every visitor is reminded that they have the power to save lives, therefore let's use it right now, right here. Please register as a blood donor right away if you are eligible!</p>
+			<p class="top">We'd also like to thank our entire team for all of their contributions, dedication, and hard work in making our dream a reality. We'd want to thank all of our supporters and well-wishers for their encouragement and support throughout this effort. It's safe to claim that, as a group, we've made this society a little bit better and safer to live in.</p>
+		<p class="top">Thank you and Happy Blood donating!</p>	
+        <p class="top"> Ahmed El-Sayed</p>
+<p>Abdulrahman Yasser</p>
+<p>Mohammed Ashraf</p>
+<p>Mohammed Ayman</p>
+        <p class="top">Promoters,</p>
+<p>Blood Bank Egypt.</p>
+       
+	</div>
+	<div class="sidebar">
+			<div>  
+<br /><br /><br /><br /><br />
+			       <div>
+                       <img src="Images/doc.png" width="250px" height="200px" class="tableborder" />
+						<br /><br />
+                            <img src="Images/camp1.png" width="250px" height="500px" class="tableborder" />
+                            
+				  </div>
+				 
+					 <div class="clear"></div>	
+				</div>	
+	</div>
+	
+</div>
+</div>
+		
+</form>
+</div>
+
+       
+      
+        <div class="clear"></div>
+<div class="ftr-bg">
+<div class="wrap">
+<div class="footer">
+	<div class="f_nav">
+	</div>
+		<div class="copy">
+			<p class="title">All Rights Reserved </p>
+		</div>
+	<div class="clear"></div>
+</div>
+</div>
+</div>
+</div>
+
+</body>
+</html>
