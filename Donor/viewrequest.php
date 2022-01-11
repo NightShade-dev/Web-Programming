ce18071920b27521415e2bcd220e5675e298a3da
NightShade-dev <78556171+NightShade-dev@users.noreply.github.com>
2022-01-11 23:06:52 +0200

    Add files via upload

---
+++ Donor/viewrequest.php
@@ -0,0 +1,66 @@
+<?php if(!isset($_SESSION)) {session_start();}  ?>
+
+<!DOCTYPE>
+<html>
+<head>
+<title>View Requestes</title>
+
+</head>
+
+<body>
+
+<?php include('connection.php'); ?>
+
+
+		<div class="logo">
+			<h1><a href="index.php"><img src="Images/logo.png" alt=""></a></h1>
+		</div>
+	
+	<ul class="nav">
+			<li class="active"><a href="chngpwd.php">Change Password</a></li>	
+			<li><a href="updateprofile.php">Update Profile</a></li>            
+			<li><a href="blooddonated.php">Blood Donated</a></li>
+            <li><a href="viewdonations.php">View Donations</a></li>
+            <li><a href="viewrequest.php">View Requestes</a></li>
+            <li><a href="logout.php">log Out</a></li>
+           
+            </ul>
+	<
+<div style="height:300px; width:1000px; margin:auto; margin-top:50px; margin-bottom:50px; background-color:#f8f1e4; border:2px solid red; box-shadow:4px 1px 20px black;">
+     <form method="post">
+ <table cellpadding="20" cellspacing="20" width="1000px" height="200px"  style="margin:auto" >
+
+   <tr><td colspan="7" align="center"><img src="Images/brequest.png" height="90px" /></td></tr>
+
+   <tr><td>&nbsp;</td><td>&nbsp;</td></tr>   
+ <tr style="background-color:bisque" align="center" class="bold">            
+             <td class="bold" style="color:red" align="center">Name</td><td align="center">Gender</td><td align="center">Contact No</td><td align="center">Mobile No</td><td align="center">Email</td>
+            <td align="center">Till Required Date</td>
+        </tr>
+                   
+
+
+
+<?php
+
+	
+$cn=mysqli_connect("localhost","root","","bloodbank");
+$s="select * from requestes";
+	$result=mysqli_query($cn,$s);
+	$r=mysqli_num_rows($result);
+	//echo $r;
+	while($data=mysqli_fetch_array($result))
+	{
+				echo"<td  style=' padding-left:10px'>$data[1]</td><td  style=' padding-left:20px'>$data[2]</td><td  style=' padding-left:30px'>$data[3]</td><td  style=' padding-left:50px'>$data[4]</td><td  style=' padding-left:50px'>$data[5]</td><td  style=' padding-left:60px'>$data[6]</td></tr>";
+			}
+			mysqli_close($cn);
+			?>
+
+
+</table>
+</form>
+        </div>
+          		
+	
+</body>
+</html>
