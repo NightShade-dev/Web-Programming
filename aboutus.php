a2089edfa1db3ea7996ea752cad33ed2bf8bd364
Abdo Yasser <61123689+thehohoman@users.noreply.github.com>
2022-01-12 00:12:22 +0200

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html5>
+<head>
+
+<title>Blood bank Management System</title>
+<link rel="stylesheet" href="Css.css">
+</head>
+
+<body>
+
+		<div class="logo">
+			<h1><a href="index.php"><img src="Images/logo.png" alt=""></a></h1>
+		</div>
+	<div>
+		<?php require('header.php');?>
+		<hr>
+	</div>
+    <br>
+   
+<div style="height:500px;">
+		<img src="Images/about.png" height="70px"  />
+			<h4>News Letter from the Founders</h4>
+			<p>The 'Life Saver Blood Bank' is the first product to emerge from 'SV Technologies' community welfare effort, the 'Web Development Project.' 'Blood' is universally acknowledged as the most valuable ingredient that maintains life. It saves countless lives in a variety of situations all around the world. Someone, somewhere, is in severe need of blood once every two seconds. Every year, more than 29 million units of blood components are transfused. Blood is in high demand; roughly 39,000 units of Red Blood Cells are required on any given day. We could only meet about 1% (approximately) of our country's blood transfusion demand each year.</p>
+			<p>Despite an increase in donor numbers, blood is still in short supply during emergencies, owing to a lack of awareness and accessibility. We are confident that by efficiently connecting blood donors with blood recipients, this technology will be able to address the majority of these issues. Every visitor is reminded that they have the power to save lives, therefore let's use it right now, right here. Please register as a blood donor right away if you are eligible!</p>
+			<p>We'd also like to thank our entire team for all of their contributions, dedication, and hard work in making our dream a reality. We'd want to thank all of our supporters and well-wishers for their encouragement and support throughout this effort. It's safe to claim that, as a group, we've made this society a little bit better and safer to live in.</p>
+		<p>Thank you and Happy Blood donating!</p>	
+        <p> Ahmed El-Sayed</p>
+<p>Abdulrahman Yasser</p>
+<p>Mohammed Ashraf</p>
+<p>Mohammed Ayman</p>
+        <p>Promoters,</p>
+              <p>Blood Bank Egypt.</p>
+       </div>
+
+
+
+</body>
+</html>
