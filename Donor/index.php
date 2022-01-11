8dfd9e9be436143a1db5503decba57494555af44
Abdo Yasser <61123689+thehohoman@users.noreply.github.com>
2022-01-12 00:01:19 +0200

    Add files via upload

---
+++ Donor/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html >
+<html>
+<head>
+<title>Donor Page</title>
+<link rel="stylesheet" href="DCss.css">
+</head>
+
+<body>
+ 
+		<div class="logo">
+			<h1><a href="index.php"><img src="Images/logo.png" alt=""></a></h1>
+		</div>
+
+		<ul class="Topnav">
+		 <a href="chngpwd.php">Change Password</a>	
+		 <a href="updateprofile.php">Update Profile</a>            
+		 <a href="blooddonated.php">Blood Donated</a>
+           <a href="viewdonations.php">View Donations</a>
+           <a href="viewrequest.php">View Requestes</a>
+           <a href="logout.php">log Out</a>
+           
+            </ul>
+	    <hr>
+	    <br>
+<div class="IndexPage">
+    <img src="Images/donorpannel.png"/>
+        
+			
+			
+	</div>
+	
+</body>
+</html>
