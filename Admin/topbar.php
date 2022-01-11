4ed489afcaf1ae69b705a21f47d66998156ecbb9
Abdo Yasser <61123689+thehohoman@users.noreply.github.com>
2022-01-12 00:02:03 +0200

    Add files via upload

---
+++ Admin/topbar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Top Bar</title>
+</head>
+
+<body>
+
+		<div class="logo">
+			<h1><img src="images/logo.png" alt=""></h1>
+		</div>
+	
+		<ul class="nav">
+			
+            <li><a href="../index.php" target="_blank">Preview Website</a></li>
+             <li><a href="logout.php">Log Out</a></li>
+			<li>
+              
+            </li>
+            </ul>
+	</body>
+</html>
