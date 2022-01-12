4ac578f3988beb1ca5be8686c0487489296cb129
NightShade-dev <78556171+NightShade-dev@users.noreply.github.com>
2022-01-12 07:50:05 +0200

    Add files via upload

---
+++ Donor/soon.php
@@ -0,0 +1,43 @@
+<?php session_start();  ?>
+<!DOCTYPE html5>
+<head>
+<title>Blood bank Management System</title>
+<link rel="stylesheet" href="DCss.css">
+<link rel="stylesheet" href="soon.css">
+</head>
+
+<body>
+
+
+		<div class="logo">
+			<h1><a href="index.php"><img src="Images/logo.png" alt=""></a></h1>
+		</div>
+
+		<ul class="Topnav">
+		 <a href="chngpwd.php">Change Password</a>	
+		 <a href="updateprofile.php">Update Profile</a>            
+		 <a href="blooddonated.php">Donate Blood</a>
+           <a href="viewdonations.php">View Donations</a>
+           <a href="viewrequest.php">View Requestes</a>
+ <a href="soon.php">Coming Soon </a>
+           <a href="logout.php">log Out</a>
+           
+            </ul>
+
+
+<br> <br>
+		<div class="bgimg">
+		
+  <div class="middle">
+    <h1>More Features COMING SOON</h1>
+    <hr>
+    <p>1 Year</p>
+   <hr>
+    <p id="demo" style="font-size:30px"></p>
+  </div>
+</div>
+
+<script src="soon.js"></script>
+
+</body>
+</html>
