b73c64e6f5753b7e3bb4b858184f122f21eeeb28
NightShade-dev <78556171+NightShade-dev@users.noreply.github.com>
2022-01-12 07:50:28 +0200

    Add files via upload

---
+++ Admin/soon.php
@@ -0,0 +1,29 @@
+<?php session_start();  ?>
+<!DOCTYPE html5>
+<head>
+<title>Blood bank Management System</title>
+<link rel="stylesheet" href="ACss.css">
+<link rel="stylesheet" href="soon.css">
+</head>
+
+<body>
+
+
+		<?php require('topbar.php');?>
+
+<br> <br>
+		<div class="bgimg">
+		
+  <div class="middle">
+    <h1>More Features COMING SOON</h1>
+    <hr>
+    <p>1 Year</p>
+   <hr>
+    <p id="demo" style="font-size:30px"></p>
+  </div>
+</div>
+
+<script src="soon.js"></script>
+
+</body>
+</html>
